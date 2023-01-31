582756480a3b71d33c6772a3c7e2504b106d49d7
abhit2109 <122540059+abhit2109@users.noreply.github.com>
2023-01-31 10:19:03 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,28 @@
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    # . # . #
+    # . # . #
+    # . # . #
+    `)
+basic.clearScreen()
+basic.pause(500)
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    . # . # .
+    . # . # .
+    . . # . .
+    `)
+basic.clearScreen()
+basic.pause(500)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    # . # . #
+    # . # . #
+    # . # . #
+    `)
+basic.clearScreen()
+basic.pause(500)
+basic.showString("CAN I GET SOME PIZZA ROLLS")
